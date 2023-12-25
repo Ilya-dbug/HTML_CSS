19fb2a8c46efece3a58c6e9a87ece3b87420adc3
Ilya-dbug <66306007+Ilya-dbug@users.noreply.github.com>
2023-12-25 03:24:53 +0300

    Add files via upload

---
+++ homework_4/MyHttpServer/MyHttpServer/Configuration/ServerConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace MyHttpServer.Configuration
+{
+    public class ServerConfiguration
+    {
+        private const string _configFilePath = "appsettings.json";
+        private AppSettings? _config;
+
+        public ServerConfiguration()
+        {
+            _config = new AppSettings();
+        }
+
+        public void Set(HttpListener httplistener)
+        {
+            try
+            {
+                if (!File.Exists(_configFilePath))
+                {
+                    Console.WriteLine("json file not found!");
+                    throw new Exception();
+                    throw new FileNotFoundException("json file not found!");
+                }
+
+                using (FileStream file = File.OpenRead(_configFilePath))
+                {
+                    _config = JsonSerializer.Deserialize<AppSettings>(file);
+                }
+
+                httplistener.Prefixes.Add($"{_config.Address}:{_config.Port}/");
+                httplistener.Prefixes.Add($"http://localhost:{_config.Port}/");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Configuration: {ex.Message}");
+                Console.ResetColor();
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Configuration: All configurations set!");
+                Console.ResetColor();
+            }
+        }
+
+        public string StaticFilesPath
+        {
+            get { return _config.StaticFilesPath; }
+            set
+            {
+                if (!Directory.Exists(_config.StaticFilesPath))
+                {
+                    Directory.CreateDirectory(_config.StaticFilesPath);
+                }
+            }
+        }
+    }
+}
---
+++ homework_4/MyHttpServer/MyHttpServer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyHttpServer
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            Server server = new Server();
+            await server.Start();
+        }
+    }
+}
